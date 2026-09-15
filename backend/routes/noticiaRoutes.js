dfb95c4c046ab2b9034de5167991c2cf49c48bbf
Seynng09 <s2angelo9876@gmail.com>
2026-09-15 11:10:37 -0500

    debug: anade logs para diagnosticar que argumento es una Promise

--- backend/routes/noticiaRoutes.js
+++ backend/routes/noticiaRoutes.js
@@ -1,11 +1,18 @@
 ﻿const express = require('express');
 const router = express.Router();
+
+console.log('noticiaRoutes: cargando controlador...');
 const noticiaController = require('../controllers/noticiaController');
+console.log('noticiaRoutes: crearNoticia es:', typeof noticiaController.crearNoticia);
+
 const { verificarToken, soloAdmin } = require('../middleware/authMiddleware');
 const { upload, verifyMagicBytes, processAndUpload } = require('../middleware/uploadMiddleware');
 const { handleValidation } = require('../middleware/validate');
 const { noticia: validateNoticia, idParam } = require('../validators/schemas');
 const { sanitizeBody } = require('../utils/sanitize');
+
+console.log('noticiaRoutes: validateNoticia es:', Array.isArray(validateNoticia) ? `array[${validateNoticia.length}]` : typeof validateNoticia);
+console.log('noticiaRoutes: processAndUpload("noticias") es:', typeof processAndUpload('noticias'));
 
 const uploadNoticia = [
   upload.single('imagen'),
@@ -37,4 +44,3 @@ router.put(
 router.delete('/:id', verificarToken, soloAdmin, ...idParam, handleValidation, noticiaController.eliminarNoticia);
 
 module.exports = router;
-
